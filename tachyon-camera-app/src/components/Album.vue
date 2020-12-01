385966b6df82ac0efdb53d2c8911085e9e567664
Brian Winkers <bwinkers@gmail.com>
2020-12-01 01:07:34 -0800

    Working upload from files and camera

--- tachyon-camera-app/src/components/Album.vue
+++ tachyon-camera-app/src/components/Album.vue
@@ -38,21 +38,49 @@
   
     </FormulateForm>
 
-<amplify-s3-image-picker />
+    <FormulateInput
+        type="image"
+        name="headshot"
+        label="Select an image to upload"
+        help="Select a png, jpg or gif to upload."
+        validation="mime:image/jpeg,image/png,image/gif"
+        :uploader="uploadToS3"
+    />
 
+<amplify-s3-image-picker button-text="Choose "
+                    header-title="CMS File Uploader"
+                    title="Photo Upload"
+                    path="uploads/"
+                    track=true
+                    identity-id="brian"
+                     />
 
+ <amplify-photo-picker v-bind:photoPickerConfig="photoPickerConfig"></amplify-photo-picker>
 
-   
+<amplify-s3-album path="uploads/"></amplify-s3-album>
 
     </div>
 </div>
 </template>
 
 <script>
-import { API, Auth } from 'aws-amplify'
+import Amplify, { API, Auth, Storage } from 'aws-amplify'
+import awsconfig from '@/aws-exports';
+Amplify.configure(awsconfig);
+
 import { updateAlbum } from '@/graphql/mutations'
 import { getAlbum } from '@/graphql/queries'
+import { v4 as uuidv4 } from 'uuid';
 
+const photoPickerConfig = {
+  path: 'uploads/',
+  headerTitle: 'Upload to CMS',
+  buttonText: 'Upload',
+  storageOptions: {
+      level: 'private',
+      ExtraArgs: {"Metadata": {"mykey": "myvalue"}}
+  }	
+}
 
 export default {
     name: 'Album',
@@ -65,7 +93,8 @@ export default {
         return {
             user: {},
             formValues: {},
-            album: {}
+            album: {},
+            photoPickerConfig
         }
     },
     beforeCreate() {
@@ -96,7 +125,40 @@ export default {
             });
             //this.$formulate.reset('edit-album')
 
+        },
+        async uploadFile(file) {
+            const fileName = 'upload/'+uuidv4()
+            const user = await Auth.currentAuthenticatedUser();
+
+            const result = await Storage.vault.put(
+            fileName, 
+            file, 
+            {
+                metadata: {
+                albumid: this.albumId,
+                owner: user.username,
+                }
+            })
+
+             console.log('Uploaded file: ', result);
+        },
+        async uploadToS3 (file, progress, error, option) {
+            //const user = await Auth.currentAuthenticatedUser();
+            console.log('made it' + file + progress + error + option)
+            const uid = await uuidv4()
+            console.log(uid)
+
+            const fileName = 'uploads/' + uid 
+
+            await Storage.put(fileName, file, {
+                progressCallback(progress) {
+                    console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+                },
+            })
+            .then (result => console.log(result))
+            .catch(err => console.log(err));
         }
     }
-}
-</script>
+  }
+
+  </script>
--- tachyon-camera-app/src/components/Camera.vue
+++ tachyon-camera-app/src/components/Camera.vue
@@ -47,11 +47,13 @@
 </template>
 
 <script>
-import {
-    Auth
-} from 'aws-amplify'
+import Amplify, { Auth, Storage } from 'aws-amplify'
+import awsconfig from '@/aws-exports';
+Amplify.configure(awsconfig);
 
 import { WebCam } from "vue-web-cam";
+
+import { v4 as uuidv4 } from 'uuid';
 
 
 export default {
@@ -97,6 +99,8 @@ export default {
     methods: {
         onCapture() {
             this.img = this.$refs.webcam.capture();
+            console.log(this.img)
+            this.uploadToS3 (this.img)
         },
         onStarted(stream) {
             console.log("On Started Event", stream);
@@ -121,6 +125,41 @@ export default {
             this.deviceId = deviceId;
             this.camera = deviceId;
             console.log("On Camera Change Event", deviceId);
+        },
+        async uploadToS3 (file, progress, error, option) {
+            //const user = await Auth.currentAuthenticatedUser();
+            console.log('made it' + file + progress + error + option)
+            const uid = await uuidv4()
+            console.log(uid)
+            const image = await this.dataURItoBlob(file)
+            const fileName = 'uploads/' + uid + '.jpeg'
+
+            await Storage.put(fileName, image, {
+                progressCallback(progress) {
+                    console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+                },
+            })
+            .then (result => console.log(result))
+            .catch(err => console.log(err));
+        },
+        async dataURItoBlob(dataURI) {
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                byteString = atob(dataURI.split(',')[1]);
+            else
+                byteString = unescape(dataURI.split(',')[1]);
+
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {type:mimeString});
         }
     }
 }
